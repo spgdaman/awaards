fbddac20f3c3a43306b3b4cb38efd1009f318527
simon <gatheru.simon432@gmail.com>
2019-05-25 19:29:18 +0300

    search functionality added

--- awaards/urls.py
+++ awaards/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name="hello"),
+    url(r'^search/', views.search_results, name="search"),
 ]
 
 if settings.DEBUG:
--- awaards/views.py
+++ awaards/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from .models import Profile,Project
+from django.views.decorators.csrf import csrf_protect
 
 def index(request):
     projects = Project.objects.all()
     return render(request, 'index.html', {"projects":projects})
 
+@csrf_protect
+def search_results(request):
+    c={}
+    if 'projects' in request.GET and request.GET['[projects]']:
+        search_term = request.GET.get('projects')
+        search_item = Project.objects.filter(name__icontains=search_term)
+        message=f"{{search_term}}"
+        return render(request,'search.html',{"message":message,"projects":search_item},c)
+    else:
+        message="Please enter a correct search term"
+        return render(request,"search.html",c)
