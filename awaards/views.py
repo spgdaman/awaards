1ca7b4e04641e3b116f12d722ac8510745e714f7
simon <gatheru.simon432@gmail.com>
2019-05-25 10:57:37 +0300

    navbar created and included to the base templete

--- awaards/views.py
+++ awaards/views.py
@@ -2,4 +2,4 @@ from django.http import HttpRequest
 from django.shortcuts import render, redirect
 
 def hello(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
